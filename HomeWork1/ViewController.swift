b52ba3a73011c9a8108e917f88dad0468855aff9
Tatiana Iljina <ilyitatjana@yandex.ru>
2018-09-21 19:11:28 +0300

    Initial Commit

---
+++ HomeWork1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HomeWork1
+//
+//  Created by Татьяна Ильина on 21.09.2018.
+//  Copyright © 2018 Татьяна Ильина. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
