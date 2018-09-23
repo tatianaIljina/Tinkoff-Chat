2d3452fafc0285ade4b8845688ace6c4ff0a20dc
Tatiana Iljina <ilyitatjana@yandex.ru>
2018-09-23 22:47:31 +0300

    UIViewController and UIApp states

--- HomeWork1/AppDelegate.swift
+++ HomeWork1/AppDelegate.swift
@@ -9,38 +9,82 @@
 import UIKit
 
 @UIApplicationMain
+
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
+    func state() -> String {
+        switch UIApplication.shared.applicationState {
+        case .active:
+            let state: String = "active"
+            return state
+        case .inactive:
+            let state: String = "inactive"
+            return state
+        case .background:
+            let state: String = "background"
+            return state
+        }
+    }
+    
+    func methodName(string: String = #function) -> String {
+        return (string)
+    }
+    
+    var pastState: String = "inactive"
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
+        
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        #if DEBUG
+            print("Application moved from \(pastState) to \(state()): \(methodName())")
+            pastState = state()
+        #else
+        #endif
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        #if DEBUG
+            print("Application moved from \(pastState) to \(state()): \(methodName())")
+            pastState = state()
+        #else
+        #endif
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        #if DEBUG
+            print("Application moved from \(pastState) to \(state()):  \(methodName())")
+            pastState = state()
+        #else
+        #endif
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        #if DEBUG
+            print("Application moved from \(pastState) to \(state()): \(methodName())")
+            pastState = state()
+        #else
+        #endif
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        #if DEBUG
+            print("Application moved from \(pastState) to \(state()):  \(methodName())")
+            pastState = state()
+        #else
+        #endif
     }
-
-
 }
 
--- HomeWork1/ViewController.swift
+++ HomeWork1/ViewController.swift
@@ -9,17 +9,89 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var NameLabel: UILabel!
+    
+    func methodName(string: String = #function) {
+        print(string)
+    }
+    
+    //Срабатывает после загрузки view
     override func viewDidLoad() {
         super.viewDidLoad()
+        #if DEBUG
+            methodName()
+        #else
+        #endif
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    //Срабатывает перед появлением View на экране
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        #if DEBUG
+            methodName()
+        #else
+        #endif
+    }
+    
+    //Говорит, что размер view будет изменен под размер экрана
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        #if DEBUG
+            methodName()
+        #else
+        #endif
+    }
+    
+    //Говорит, что размер view изменился
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        #if DEBUG
+            methodName()
+        #else
+        #endif
+    }
+    
 
+    //Срабатывает после отоборажения view на экране
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        #if DEBUG
+            methodName()
+        #else
+        #endif
+    }
+    
+    //Переворот экрана command+стрелка
+//    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+//        super.viewWillTransition(to_size, with_coordinator)
+//        print("viewWillTransition")
+//    }
+    
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        #if DEBUG
+            super.viewDidDisappear(animated)
+            methodName()
+        #else
+        #endif
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        #if DEBUG
+            methodName()
+        #else
+        #endif
+    }
+    
+    //Срабатывает при нехватке памяти, когда приложение может быть закрыто, можно отправить пуш уведомление
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 
 }
 
