30bee1cced111abb362f5cd3c3f978345144b3f8
Tatiana Iljina <ilyitatjana@yandex.ru>
2018-09-24 19:35:08 +0300

    Method was changed

--- HomeWork1/AppDelegate.swift
+++ HomeWork1/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func state() -> String {
+        #if DEBUG
         switch UIApplication.shared.applicationState {
         case .active:
             let state: String = "active"
@@ -26,8 +27,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             let state: String = "background"
             return state
         }
+        #else
+        #endif
     }
-    
+
     func methodName(string: String = #function) -> String {
         return (string)
     }
@@ -43,48 +46,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-        #if DEBUG
             print("Application moved from \(pastState) to \(state()): \(methodName())")
             pastState = state()
-        #else
-        #endif
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-        #if DEBUG
             print("Application moved from \(pastState) to \(state()): \(methodName())")
             pastState = state()
-        #else
-        #endif
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-        #if DEBUG
             print("Application moved from \(pastState) to \(state()):  \(methodName())")
             pastState = state()
-        #else
-        #endif
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-        #if DEBUG
             print("Application moved from \(pastState) to \(state()): \(methodName())")
             pastState = state()
-        #else
-        #endif
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        #if DEBUG
             print("Application moved from \(pastState) to \(state()):  \(methodName())")
             pastState = state()
-        #else
-        #endif
     }
 }
 
